548c509492a7461cd1c038791442c45fe768fc46
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-07-14 21:32:05 +0600

    auth client update

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,10 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
+
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_BETTER_AUTH_URL,
-    
-})
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "http://localhost:7000",
+});
 
 export const {
   signIn,
