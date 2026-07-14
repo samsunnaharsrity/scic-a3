67d28b9b8bd8379fce3b3dd15568072f7ca6b3ff
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-07-14 20:51:42 +0600

    user suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import ExploreSection from "./components/explore/exploreSec";
 import CategoryGrid from "./components/explore/categoryGrid";
 import FeaturedStays from "./components/featuredStays";
 import TravelBlogs from "./components/travelBlogs";
+import { Suspense } from "react";
 
 
 
@@ -16,7 +17,9 @@ export default function Home() {
       <Hero></Hero>
       <HomeAbout></HomeAbout>
       <CategoryGrid limit={4}/>
-      <ExploreSection limit={4}></ExploreSection>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ExploreSection limit={4} />
+      </Suspense>
       {/* <FeaturedStays /> */}
       {/* <Testimonials /> */}
       <TravelBlogs />
